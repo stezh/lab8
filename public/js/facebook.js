d95636e336acd95b8953e5995b089efe960b5ffe
Steve Zhao <xizhao@ucsd.edu>
2017-03-02 09:51:50 -0800

    thirdpartyAPI lab

---
+++ public/js/facebook.js
@@ -0,0 +1,19 @@
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function statusChangeCallback(response) {
+  if (response.status === 'connected') {
+    console.log('Successfully logged in with Facebook');
+    FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
+  }
+}
+
+function changeUser(response) {
+  console.log(response)
+  $('.facebookLogin').hide();
+  $('#name').text(response.name);
+  $('#photo').attr('src', response.picture.data.url);
+}
